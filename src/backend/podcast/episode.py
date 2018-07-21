c45cad9b87e5abde15d382128b01c9ec65489a5f
Dmitry Logvinenko <5diezfun@gmail.com>
2018-07-21 09:53:59 +0300

    ~ Big Episode refactoring:
    - Tag quilifers moved from Cofig to class
    ~ Fields renaming
    ~ logging instead of print
    
    ~ Test fixed

--- src/backend/podcast/episode.py
+++ src/backend/podcast/episode.py
@@ -1,80 +1,55 @@
-import builtins
 import logging
 
-from bs4 import BeautifulSoup
+import dateutil.parser
 import requests
-
-from config import CONFIG
-
-
-log = logging.getLogger(__name__)
+from bs4 import BeautifulSoup as bs
 
 
 class Episode:
-    def __init__(self, url):
-        self.url = url
+    class Qualifiers:
+        song = ['span', {'class': 'toc-text'}]
+        id = ['a', {'class': 'volume-link'}]
+        title = ['h1', {'class': 'post-title'}]
+        date = ['time', {'itemprop': 'datePublished'}]
 
-        self.header = None
+    def __init__(self, url=None):
+        self.url = None
+
+        self.id = None
+        self.title = None
         self.date = None
-        self.volume = None
+
+        self.raw_tracks = []
         self.tracks = []
 
-        self.load()
+        if url:
+            self.load(url)
 
     def __repr__(self):
         return (
-            '<self.__class__.__name__ ' +
-            'header: "self.header" ' +
-            'date: "self.date" ' +
-            'volume: "self.volume" ' +
-            'volume: "self.tracks"' +
-            '>'
-            ).format(self=self)
-
-    def __str__(self):
-        return repr(self)
+            f'<{self.__class__.__name__} '
+            f'id: {self.id}, '
+            f'title: "{self.title}", '
+            f'date: "{self.date}", '
+            f'tracks: "{self.tracks}">'
+            )
 
     def load(self, url=None):
         if url:
+            logging.debug(f'Url got: {url}')
             self.url = url
 
-        class_name = self.__class__.__name__
-        fields = map(str.strip, CONFIG.get(class_name, 'fields').split(','))
+        logging.debug(f'{self.url} loading...')
 
-        for field in fields:
-            def get(suffix):
-                """ Curring of config.get for current field
-                """
-                return CONFIG.get(class_name, '{}_{}'.format(field, suffix), fallback=None)
+        page = requests.get(url)
+        soup = bs(page.text, 'html.parser')
 
-            # Get values from config
-            source = get('source')
-            tag = get('tag')
-            class_ = get('class')
-            type_ = get('type')
-            if source == 'attr':
-                attr = get('attr')
+        self.id = int(soup.find(*self.Qualifiers.id).text)
+        self.title = soup.find(*self.Qualifiers.title).text.strip()
+        self.date = dateutil.parser.parse(soup.find(*self.Qualifiers.date).text.strip()).date()
 
-            # Parse page
-            req = requests.get(self.url)
-            data = req.text
-            bs = BeautifulSoup(data, 'html.parser')
+        nodes = soup.find_all(*self.Qualifiers.song)
+        self.raw_tracks = [node.string for node in nodes]
 
-            # Find values
-            for node in bs.find_all(tag, class_=class_):
-                if source == 'string':
-                    value = node.string
-                elif source == 'attr':
-                    value = node.get(attr)
 
-                # Trim spaces
-                value = value.strip()
-
-                # Convert to specific data type if needed
-                if type_:
-                    value = getattr(builtins, type_)(value)
-
-                if isinstance(getattr(self, field), list):
-                    getattr(self, field).append(value)
-                else:
-                    setattr(self, field, value)
+        logging.debug(f'Loaded: "{self.raw_tracks}"')
--- src/backend/podcast/test_episode.py
+++ src/backend/podcast/test_episode.py
@@ -1,6 +1,12 @@
+import datetime
+import logging
+import sys
 import unittest
 
 from episode import Episode
+
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 class EpisodeTest(unittest.TestCase):
@@ -9,5 +15,7 @@ class EpisodeTest(unittest.TestCase):
         print(self.episode)
 
     def test_load(self):
-        self.assertEqual('Золотой век музыки - 1', self.episode.header)
-        self.assertEqual(1, self.episode.volume)
+        self.assertEqual(1, self.episode.id)
+        self.assertEqual('Золотой век музыки - 1', self.episode.title)
+        self.assertEqual(datetime.date(2005, 5, 22), self.episode.date)
+        self.assertIsInstance(self.episode.raw_tracks, list)

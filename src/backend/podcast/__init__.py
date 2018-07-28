6ea5ffadf64c19d6d17429b5ba70ddf95ae7e373
Dmitry Logvinenko <5diezfun@gmail.com>
2018-07-28 13:44:56 +0300

    + Podcast + Podcast Test

---
+++ src/backend/podcast/__init__.py
@@ -0,0 +1,33 @@
+import urllib.parse
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+import episode
+
+
+class Podcast:
+    class Qualifiers:
+        episode = ['a', {'class', 'archive-post-title'}]
+
+    def __init__(self, url=None):
+        self.url = None
+
+        self.episodes = []
+        self.artists = []
+
+        if url:
+            self.load(url)
+
+    def load(self, url=None):
+        if url:
+            self.url = url
+
+        page = requests.get(url)
+        soup = bs(page.text, 'html.parser')
+
+        nodes = soup.find_all(*self.Qualifiers.episode)
+        for node in nodes:
+            url = node.get('href')
+            abs_url = urllib.parse.urljoin(self.url, url)
+            self.episodes.append(episode.Episode(abs_url))

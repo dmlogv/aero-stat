2c6a3edb8a5981dda1e89dd408db30d61f54b5fd
Dmitry Logvinenko <5diezfun@gmail.com>
2018-07-28 13:45:50 +0300

    + Track init + Track Test

--- src/backend/podcast/track.py
+++ src/backend/podcast/track.py
@@ -1,5 +1,16 @@
 class Track:
-    def __init__(self, name, artist):
-        self.name = name
-        self.artist = artist
+    def __init__(self, raw=None):
+        self.raw = None
+
+        self.name = None
+        self.artist = None
         self.length = None
+
+        if raw:
+            self.parse(raw)
+
+    def parse(self, raw=None):
+        if raw:
+            self.raw = raw
+
+        self.artist, self.name = self.raw.split(' - ')

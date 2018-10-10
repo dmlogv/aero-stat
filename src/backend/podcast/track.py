baeba9b0fb9d894ed0cdcc28d074c155c3f1642d
Dmitry Logvinenko <5diezfun@gmail.com>
2018-10-10 23:39:06 +0300

    Add a correct Track implementation
    Implement dunders
    Implement track parsing

--- src/backend/podcast/track.py
+++ src/backend/podcast/track.py
@@ -1,16 +1,39 @@
+from artist import Artist
+
+
 class Track:
-    def __init__(self, raw=None):
+    def __init__(self, artists, raw=None):
         self.raw = None
 
         self.name = None
         self.artist = None
+
         self.length = None
 
         if raw:
-            self.parse(raw)
+            self.parse(artists, raw)
 
-    def parse(self, raw=None):
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
+    def __hash__(self):
+        return hash((self.name, self.artist))
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__} "{self.artist}" — "{self.name}"'
+
+    def parse(self, artists, raw=None):
         if raw:
             self.raw = raw
 
-        self.artist, self.name = self.raw.split(' - ')
+        artist, self.name = self.raw.split(' - ', maxsplit=1)
+
+        try:
+            i = artists.index(artist)
+        except ValueError:
+            a = Artist(artist)
+            artists.append(a)
+        else:
+            a = artists[i]
+
+        self.artist = a

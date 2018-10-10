7173c612fa88dedac428a63dc5c41cc548e09d4d
Dmitry Logvinenko <5diezfun@gmail.com>
2018-10-10 23:33:34 +0300

    Add Artist dunders' implementation

--- src/backend/podcast/artist.py
+++ src/backend/podcast/artist.py
@@ -2,3 +2,14 @@ class Artist:
     def __init__(self, name):
         self.name = name
         self.isband = None
+
+        self.tracks = set()
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__} "{self.name}">'
